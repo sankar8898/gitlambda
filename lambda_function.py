3eb6e48d4b9e85fd733e6ad4f6db4be69d7304f0
sankar8898 <sankaryallapu7@gmail.com>
2025-04-18 13:17:43 +0530

    code added

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,6 @@ def lambda_handler(event, context):
     d = {'col1': [1,2], 'col2': [3,4]}
     df = pd.DataFrame(data=d)
     print(df)
-    print('Done 123121')
+    print('Done 1231212')
     # print("Event received:", event)
     # print("Context received:", context)
